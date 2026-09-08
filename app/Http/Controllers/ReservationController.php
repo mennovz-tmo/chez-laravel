f256887d77c8492d4816a4cab7e60d13b0bf1a86
Menno van Zoelen <menno@themindoffice.nl>
2026-09-08 14:06:30 +0200

    feat: finished validation of reservations
    fix: remove easy inputs from form for reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
@@ -22,8 +25,8 @@ class ReservationController extends Controller
             'email' => ['required', 'email'],
             'comment' => ['nullable', 'string', 'max:1024'],
             'date' => ['required', 'date', 'date_format:Y-m-d'],
-            // 'arrival' => ['required', 'regex:((2[0-3]|[01][1-9]|10):([0-5][0-9]))'],
-            // 'arrival' => ['required', 'between:1600,2200'],
+            'arrival' => ['required', 'string', 'regex:((2[0-3]|[01][1-9]|10):([0-5][0-9]))'],
+            // 'arrival' => ['required', 'between:1600,2200'], // Would work but I don't send an integer to the server. Would require extra javascript on the client.
         ],
             [
                 'amount_of_people.max' => 'U kan niet via de form reserveren voor een groep van meer dan 10, bel het restaurant voor mogelijkheden',
@@ -36,6 +39,36 @@ class ReservationController extends Controller
         if ($input < $min || $input > $max) {
             return redirect('/reservation')->withErrors('De ingevoerde tijd is niet tussen 16:00 en 22:00');
         }
+
+        // Add seats check.
+        $chairs_used = DB::select('select sum(amount_of_people) from reservations where date = ?;', [$request->input('date')]);
+        // dd($chairs_used);
+        $chairs_used = json_decode(json_encode($chairs_used), true)[0]['sum'];  // I couldn't find any other way to turn it into an integer.
+        // dd($chairs_used);
+        // dd($chairs_used + $request->input('amount_of_people'));
+        // dd(Config::get('app.seats') - ($chairs_used + $request->input('amount_of_people')));
+        // All this could've been avoidded if I ran:
+
+        /*
+         * php artisan config:clear
+         * php artisan cache:clear
+         */
+        if (Config::get('app.seats') - ($chairs_used + $request->input('amount_of_people')) < 0) {
+            // $tmp = $chairs_used - $request->input('amount_of_people');
+            return redirect('/reservation')->withErrors('De reservering is niet gelukt, helaas hebben we deze dag geen stoelen meer!');
+        }
+
+        Reservation::fillAndInsert([
+            'number' => generate_reservation_number(),
+            'name' => $request->input('name'),
+            'amount_of_people' => $request->input('amount_of_people'),
+            'phone_number' => $request->input('phone_number'),
+            'email' => $request->input('email'),
+            'comment' => $request->input('comment'),
+            'date' => $request->input('date'),
+            'arrival' => $request->input('arrival'),
+            'departure' => new DateTime($request->input('arrival'))->modify('+120 minutes'),
+        ]);
 
         return redirect('/reservation')->with('success', 'De reservering is gelukt!');
     }
--- resources/views/reservation/create.blade.php
+++ resources/views/reservation/create.blade.php
@@ -4,19 +4,19 @@
     <form action="reservation/create" method="post">
         <div class="mb-3">
             <label for="name" class="form-label">Wat is je naam?</label>
-            <input required type="text" class="form-control" id="name" name="name" placeholder="John Doe" value="Menno">
+            <input required type="text" class="form-control" id="name" name="name" placeholder="John Doe" value="{{ old('name') }}">
         </div>
         <div class="mb-3">
             <label for="pass" class="form-label">Wat is je email?</label>
-            <input required type="email" class="form-control" name="email" id="email" placeholder="mail@example.com" value="mail@mennovz.nl">
+            <input required type="email" class="form-control" name="email" id="email" placeholder="mail@example.com" value="{{ old('email') }}">
         </div>
         <div class="mb-3">
             <label for="pass" class="form-label">Wat is je telefoon nummer?</label>
-            <input required type="tel" class="form-control" name="phone_number" id="phone_number" placeholder="06 12345678" pattern="[0-9]{2} [0-9]{8}" value="06 12345678">
+            <input required type="tel" class="form-control" name="phone_number" id="phone_number" placeholder="06 12345678" pattern="[0-9]{2} [0-9]{8}" value="{{ old('phone_number') }}">
         </div>
         <div class="mb-3">
             <label for="pass" class="form-label">Voor hoeveel mensen reserveer je?</label>
-            <input required type="number" class="form-control" name="amount_of_people" id="amount_of_people" placeholder="bijvoorbeeld: 4" min="1" max="99" value="5">
+            <input required type="number" class="form-control" name="amount_of_people" id="amount_of_people" placeholder="bijvoorbeeld: 4" min="1" max="99" value="{{ old('amount_of_people') }}">
         </div>
         <div class="mb-3">
             @php
@@ -24,11 +24,11 @@
                 $max = $min->modify('+28 days');
             @endphp
             <label for="pass" class="form-label">Voor welke datum reserveer je?</label>
-            <input required type="date" class="form-control" name="date" id="date" min="{{ $min->format('d-m-Y') }}" max="{{ $max->format('d-m-Y') }}" value="2026-09-09">
+            <input required type="date" class="form-control" name="date" id="date" min="{{ $min->format('d-m-Y') }}" max="{{ $max->format('d-m-Y') }}" value="{{ old('date') }}">
         </div>
         <div class="mb-3">
             <label for="pass" class="form-label">Welke tijd verwacht je er te zijn?</label>
-            <input required type="time" class="form-control" name="arrival" id="arrival" min="15:00" max="23:00" value="19:00">
+            <input required type="time" class="form-control" name="arrival" id="arrival" min="15:00" max="23:00" value="{{ old('arrival') }}">
         </div>
         <div class="mb-3">
             <label for="pass" class="form-label">Opmerking(en) over de reservering:</label>
