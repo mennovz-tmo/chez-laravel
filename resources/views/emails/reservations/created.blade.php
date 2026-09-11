2b3ea8f748be1da01cc27fe8ed45843b29dbbd26
Menno van Zoelen <menno@themindoffice.nl>
2026-09-11 15:06:34 +0200

    fix: emails looking broken.

--- resources/views/emails/reservations/created.blade.php
+++ resources/views/emails/reservations/created.blade.php
@@ -1,28 +1,26 @@
 <x-mail::message>
-    # Reservering ontvangen
+# Reservering ontvangen
 
-    Hallo {{ $reservation->name }},
+Hallo {{ $reservation->name }},
 
-    Dankjewel voor de reservering!
+Dankjewel voor de reservering!
 
-    Je hebt gereserveerd onder de naam {{ $reservation->name }} voor {{ $reservation->amount_of_people }} personen. De
-    datum voor je reservering is {{ $reservation->date->format('d-m-Y') }} om {{ $reservation->arrival }} uur.
+Je hebt gereserveerd onder de naam {{ $reservation->name }} voor {{ $reservation->amount_of_people }} personen. De datum voor je reservering is {{ $reservation->date->format('d-m-Y') }} om {{ $reservation->arrival }} uur.
 
-    Het opgegeven telefoon nummer is {{ $reservation->phone_number }}, dit gebruiken wij alleen om je te bereiken als er
-    een belangrijke mededeling is.
+Het opgegeven telefoon nummer is {{ $reservation->phone_number }}, dit gebruiken wij alleen om je te bereiken als er een belangrijke mededeling is.
 
-    @if($reservation->comment)
-        En als laatste is de opmerking bij de reservering:
-        {{ $reservation->comment }}
-    @endif
+@if($reservation->comment)
+En als laatste is de opmerking bij de reservering: <br>
+{{ $reservation->comment }}
+@endif
 
-    Mocht er toch nog iets zijn kunt u ons altijd bellen op 06 12345678.
-    Of mailen naar contact@chezlaravel.com
+Mocht er toch nog iets zijn kunt u ons altijd bellen op 06 12345678.
+Of mailen naar contact@chezlaravel.com
 
-    <x-mail::button :url="url('/reservation/' . $reservation->id . '/show')">
-        Bekijk reservering
-    </x-mail::button>
+<x-mail::button :url="url('/reservation/'.$reservation->id.'/show')">
+Bekijk reservering
+</x-mail::button>
 
-    Met vriendelijke groet, <br>
-    {{ config('app.name') }}
+Met vriendelijke groet, <br>
+{{ config('app.name') }}
 </x-mail::message>
