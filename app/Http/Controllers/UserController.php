39f431aa7c3225e6346a252ba94a9856ffa69aef
Menno van Zoelen <menno@themindoffice.nl>
2026-09-17 11:07:54 +0200

    fix: if you edit owner account it would make it a staff or consumer account

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::get()->sortDesc()->toArray();
+        $users = User::where('role', '!=', 'owner')->get()->sortDesc()->toArray();
 
         return view('manage.users.view')->with('users', $users);
     }
